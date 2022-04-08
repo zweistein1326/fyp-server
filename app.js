a7eada9a4db50852bb5e47b8d115e2cd9a63dcae
Siddharth Agarwal <siddharthagarwal@Siddharths-MacBook-Pro.local>
2022-04-08 14:44:34 +0530

    update hostname

--- app.js
+++ app.js
@@ -6,23 +6,25 @@ const cors = require('cors');
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config()
 }
-
+const hostname = '127.0.0.1';
 const port = process.env.PORT || 8000;
 
-app.use(fileUpload());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-
+app.use(require('./routes'));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control_Allow-Headers', "x-access token, Origin, Content-Type, Accept");
     res.setHeader("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTYzNjQ5MTkyNzA5MiIsImVtYWlsIjoiY0BjIiwiaWF0IjoxNjM2NDkxOTI3LCJleHAiOjE2MzY0OTkxMjd9.3nKmcqEh9NSx8qLX-OaVjOqIeTSBFk4BjQxELrQf1O4");
     next();
 })
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-app.use(require('./routes'));
+app.get('/',(req,res,next)=>{
+    return res.send(`Server running at Port ${port}`)
+})
 
 app.listen(port, hostname, () => {
-    console.log(`Server running on Port ${port}`);
+    console.log(`Server running at Port ${port}`);
 });
