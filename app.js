2f6f12cac2e5eb555843845491e87c28ac28688c
Siddharth Agarwal <siddharthagarwal@Siddharths-MacBook-Pro.local>
2022-04-08 14:24:41 +0530

    update base

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use((req, res, next) => {
 })
 
 app.use(require('./routes'));
+app.get('/',(req,res,next)=>{
+    return res.json(`Server running at http://${hostname}:${port}/`)
+})
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
